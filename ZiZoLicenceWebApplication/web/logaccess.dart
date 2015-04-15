a4033a89f18e6d20585ff83e2a2c1e6180917cf7
matty-w <mattwhitmoregit@hotmail.com>
2015-04-15 15:26:52 +0100

    Export Button Added - No Functionality
    Export button added to the logAccess page. Does not do anything at the
    moment.

--- ZiZoLicenceWebApplication/web/logaccess.dart
+++ ZiZoLicenceWebApplication/web/logaccess.dart
@@ -4,8 +4,14 @@ import 'dart:html';
 void main()
 {
   var log = new LoginAndOut();
+  InputElement export = querySelector("#export_button");
+  export.disabled == true;
+  
   querySelector("#logoutButton").onClick.listen(log.logout);
   querySelector("#username-output").innerHtml = window.sessionStorage['username'];
+  
 }
+
+
 
 
