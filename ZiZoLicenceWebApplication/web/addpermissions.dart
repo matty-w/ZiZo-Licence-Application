c2bc18dd3f675709444f9da8b1377e06a12fb91a
matty-w <mattwhitmoregit@hotmail.com>
2015-04-16 11:51:12 +0100

    Licence Permissions + Descriptions
    Added the extra permissions to the drop down box. Short descriptions of
    each of the permissions appears depending on which permission has been
    selected.

--- ZiZoLicenceWebApplication/web/addpermissions.dart
+++ ZiZoLicenceWebApplication/web/addpermissions.dart
@@ -1,11 +1,42 @@
 import 'loginfunctions.dart';
 import 'dart:html';
 
+
 void main()
 {
   var log = new LoginAndOut();
   querySelector("#logoutButton").onClick.listen(log.logout);
   querySelector("#username-output").innerHtml = window.sessionStorage['username'];
+  
+  setDescriptionText();
 }
 
+setDescriptionText()
+{
+  OutputElement output = querySelector("#permissionDescription");
+  output.value = "User Can Return A String Showing An Entries From Other Administrators. Includes A List Of Their Permissions.";
+  querySelector("#setPermissions").onChange.listen(setText);
+}
+
+setText(Event e)
+{
+  SelectElement dropDown = querySelector("#setPermissions");
+  OutputElement output = querySelector("#permissionDescription");
+  
+  
+  if(dropDown.value == "userEntries")
+    output.value = "User Can Return A String Showing An Entries From Other Administrators. Includes A List Of Their Permissions.";
+  else if(dropDown.value == "changePassword")
+    output.value = "Can Change Password For A User. (Note: A User Can Alter Their Own Password Without This Permission).";
+  else if(dropDown.value == "addPermissions")
+    output.value = "Can Add Indicated Operation To The List Of Things The Administrator Can Do.";
+  else if(dropDown.value == "deleteUser")
+    output.value = "User Can Delete This User From The List Of Administrators.";
+  else if(dropDown.value == "removePermissions")
+    output.value = "Can Remove Indicated Operation To The List Of Things The Administrator Can Do.";
+  else if(dropDown.value == "listCommands")
+    output.value = "Returns A Quick List Of Available Commands.";
+  else if(dropDown.value == "addUser")
+    output.value = "User Can Add A New Administrator To The Service. This Administrator Has No Permissions.";
+}
 
